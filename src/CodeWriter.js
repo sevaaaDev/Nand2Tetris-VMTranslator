9a6584ab82677ce3173617ace705909bf6c4fee9
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-11-26 15:28:56 +0700

    feat: code writer basic feature

--- src/CodeWriter.js
+++ src/CodeWriter.js
@@ -2,6 +2,9 @@ const getSegment = {
   local() {
     return "LCL";
   },
+  argument() {
+    return "ARG";
+  },
 };
 export default class CodeWriter {
   constructor() {}
@@ -20,6 +23,38 @@ M=D
 
 @SP
 A=A+1
+`;
+    }
+    if (command === "pop") {
+      return `# pop
+@${index}
+D=A
+@${getSegment[segment]()}
+D=D+M
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`;
+    }
+  }
+  writeArithmetic(command) {
+    if (command === "add") {
+      return `# add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+A=M-1
+M=D+M
 `;
     }
   }
--- src/test/CodeWriter.spec.js
+++ src/test/CodeWriter.spec.js
@@ -1,11 +1,11 @@
 import { describe, it, expect, vi } from "vitest";
 import CodeWriter from "../CodeWriter";
 
-describe("Code writer", () => {
-  it("return correct push asm", () => {
+describe("Code writer push", () => {
+  it("return correct push local asm", () => {
     const codeWriter = new CodeWriter();
     const result = codeWriter.writePushPop("push", "local", 2);
-    expect(result).toBe(`# push
+    expect(result).toBe(`# push local 2
 @2
 D=A
 @LCL
@@ -20,26 +20,54 @@ M=D
 A=A+1
 `);
   });
+  it("return correct push constant asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "constant", 2);
+    expect(result).toBe(`# push constant 2
+@2
+D=A
+
+@SP
+A=M
+M=D
+
+@SP
+A=A+1
+`);
+  });
   it("return correct pop asm", () => {
     const codeWriter = new CodeWriter();
     const result = codeWriter.writePushPop("pop", "local", 2);
     expect(result).toBe(`# pop
-
-
-@SP
-M=M-1
-
 @2
 D=A
 @LCL
 D=D+M
+@R15
+M=D
 
 @SP
+M=M-1
 A=M
-A=M
+D=M
 
+@R15
+A=M
+M=D
 `);
   });
-  it.todo("return correct add asm");
+  it("return correct add asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writeArithmetic("add");
+    expect(result).toBe(`# add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+A=M-1
+M=D+M
+`);
+  });
   it.todo("return correct and asm");
 });
