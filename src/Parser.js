3c9bd0bd1602dd757ed6468215aaf651e3b904fe
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-11-26 09:11:56 +0700

    fix: string

--- src/Parser.js
+++ src/Parser.js
@@ -2,13 +2,14 @@ import nReadLines from "n-readlines";
 
 const C_ARITHMETIC = "C_ARITHMETIC";
 const C_PUSH = "C_PUSH";
-const C_POP = "C_POP     ";
+const C_POP = "C_POP";
 const C_FUNCTION = "C_FUNCTION";
-const C_LABEL = "C_LABEL   ";
-const C_GOTO = "C_GOTO    ";
-const C_RETURN = "C_RETURN  ";
-const C_CALL = "C_CALL    ";
-const C_IF = "C_IF      ";
+const C_LABEL = "C_LABEL";
+const C_GOTO = "C_GOTO";
+const C_RETURN = "C_RETURN";
+const C_CALL = "C_CALL";
+const C_IF = "C_IF";
+
 export default class Parser {
   constructor(file) {
     this.fileLine = new nReadLines(file);
