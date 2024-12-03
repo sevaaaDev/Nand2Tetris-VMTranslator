a5234ced97deade58b8ba6f8e7060ba0889c334d
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-12-03 10:10:46 +0700

    add this that segment

--- src/CodeWriter.js
+++ src/CodeWriter.js
@@ -5,16 +5,40 @@ const getSegment = {
   argument() {
     return "ARG";
   },
+  pointer(index) {
+    if (index) return "THAT";
+    return "THIS";
+  },
+  this() {
+    return "THIS";
+  },
+  that() {
+    return "THAT";
+  },
+  temp() {
+    return "5";
+  },
 };
-export default class CodeWriter {
-  constructor() {}
-  writePushPop(command, segment, index) {
-    if (command === "push") {
-      return `# push
+
+const getAssembly = {
+  push(segment, index) {
+    if (segment === "constant") {
+      return `// push ${segment} ${index}
 @${index}
 D=A
-@${getSegment[segment]()}
-A=D+M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`;
+    }
+    if (segment === "static") {
+      // TODO: change foo
+      return `// push ${segment} ${index}
+@Foo.${index}
 D=M
 
 @SP
@@ -22,11 +46,109 @@ A=M
 M=D
 
 @SP
-A=A+1
+M=M+1
 `;
     }
-    if (command === "pop") {
-      return `# pop
+    if (segment === "pointer") {
+      return `// push ${segment} ${index}
+@${getSegment[segment](index)}
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`;
+    }
+    if (segment === "temp") {
+      return `// push ${segment} ${index}
+@${getSegment[segment]()}
+D=A
+@${index}
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`;
+    }
+    return `// push ${segment} ${index}
+@${getSegment[segment]()}
+D=M
+@${index}
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`;
+  },
+  pop(segment, index) {
+    if (segment === "static") {
+      //TODO: change foo
+      return `// pop ${segment} ${index}
+@Foo.${index}
+D=A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`;
+    }
+    if (segment === "pointer") {
+      return `// pop ${segment} ${index}
+@${getSegment[segment](index)}
+D=A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`;
+    }
+    if (segment === "temp") {
+      return `// pop ${segment} ${index}
+@${index}
+D=A
+@${getSegment[segment]()}
+D=D+A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`;
+    }
+    return `// pop ${segment} ${index}
 @${index}
 D=A
 @${getSegment[segment]()}
@@ -43,11 +165,41 @@ D=M
 A=M
 M=D
 `;
-    }
+  },
+};
+// TODO:   neg eq   gt lt
+const getAssemblyArithmetic = {
+  add() {
+    return `// add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+A=M-1
+M=D+M
+`;
+  },
+  sub() {
+    return `// sub
+@SP
+M=M-1
+A=M
+D=M
+@SP
+A=M-1
+M=D-M
+`;
+  },
+};
+export default class CodeWriter {
+  constructor() {}
+  writePushPop(command, segment, index) {
+    return getAssembly[command](segment, index);
   }
   writeArithmetic(command) {
     if (command === "add") {
-      return `# add
+      return `// add
 @SP
 M=M-1
 A=M
--- src/test/CodeWriter.spec.js
+++ src/test/CodeWriter.spec.js
@@ -5,11 +5,11 @@ describe("Code writer push", () => {
   it("return correct push local asm", () => {
     const codeWriter = new CodeWriter();
     const result = codeWriter.writePushPop("push", "local", 2);
-    expect(result).toBe(`# push local 2
-@2
-D=A
+    expect(result).toBe(`// push local 2
 @LCL
-A=D+M
+D=M
+@2
+A=D+A
 D=M
 
 @SP
@@ -17,13 +17,67 @@ A=M
 M=D
 
 @SP
-A=A+1
+M=M+1
+`);
+  });
+  it("return correct push this asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "this", 2);
+    expect(result).toBe(`// push this 2
+@THIS
+D=M
+@2
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct push that asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "that", 2);
+    expect(result).toBe(`// push that 2
+@THAT
+D=M
+@2
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct push argument asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "argument", 2);
+    expect(result).toBe(`// push argument 2
+@ARG
+D=M
+@2
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
 `);
   });
   it("return correct push constant asm", () => {
     const codeWriter = new CodeWriter();
     const result = codeWriter.writePushPop("push", "constant", 2);
-    expect(result).toBe(`# push constant 2
+    expect(result).toBe(`// push constant 2
 @2
 D=A
 
@@ -32,13 +86,76 @@ A=M
 M=D
 
 @SP
-A=A+1
+M=M+1
 `);
   });
-  it("return correct pop asm", () => {
+  it("return correct push static asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "static", 2);
+    expect(result).toBe(`// push static 2
+@Foo.2
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct push pointer 0 (this) asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "pointer", 0);
+    expect(result).toBe(`// push pointer 0
+@THIS
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct push pointer 1 (that) asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "pointer", 1);
+    expect(result).toBe(`// push pointer 1
+@THAT
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct push temp 1 asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("push", "temp", 1);
+    expect(result).toBe(`// push temp 1
+@5
+D=A
+@1
+A=D+A
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+`);
+  });
+  it("return correct pop local asm", () => {
     const codeWriter = new CodeWriter();
     const result = codeWriter.writePushPop("pop", "local", 2);
-    expect(result).toBe(`# pop
+    expect(result).toBe(`// pop local 2
 @2
 D=A
 @LCL
@@ -56,18 +173,145 @@ A=M
 M=D
 `);
   });
-  it("return correct add asm", () => {
+  it("return correct pop this asm", () => {
     const codeWriter = new CodeWriter();
-    const result = codeWriter.writeArithmetic("add");
-    expect(result).toBe(`# add
+    const result = codeWriter.writePushPop("pop", "this", 2);
+    expect(result).toBe(`// pop this 2
+@2
+D=A
+@THIS
+D=D+M
+@R15
+M=D
+
 @SP
 M=M-1
 A=M
 D=M
-@SP
-A=M-1
-M=D+M
+
+@R15
+A=M
+M=D
 `);
   });
-  it.todo("return correct and asm");
+  it("return correct pop that asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "that", 2);
+    expect(result).toBe(`// pop that 2
+@2
+D=A
+@THAT
+D=D+M
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
+  it("return correct pop argument asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "argument", 2);
+    expect(result).toBe(`// pop argument 2
+@2
+D=A
+@ARG
+D=D+M
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
+  it("return correct pop static asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "static", 2);
+    expect(result).toBe(`// pop static 2
+@Foo.2
+D=A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
+  it("return correct pop temp asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "temp", 2);
+    expect(result).toBe(`// pop temp 2
+@2
+D=A
+@5
+D=D+A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
+  it("return correct pop pointer 0 (this) asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "pointer", 0);
+    expect(result).toBe(`// pop pointer 0
+@THIS
+D=A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
+  it("return correct pop pointer 1 (that) asm", () => {
+    const codeWriter = new CodeWriter();
+    const result = codeWriter.writePushPop("pop", "pointer", 1);
+    expect(result).toBe(`// pop pointer 1
+@THAT
+D=A
+@R15
+M=D
+
+@SP
+M=M-1
+A=M
+D=M
+
+@R15
+A=M
+M=D
+`);
+  });
 });
